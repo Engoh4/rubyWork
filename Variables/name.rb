0d531ab1f677246d49338dff2d71d125eb456652
Engoh Lucienne <engohlucienne@gmail.com>
2020-03-15 23:26:53 +0100

    exercises on variables

---
+++ Variables/name.rb
@@ -0,0 +1,9 @@
+puts "Please Enter Your first name"
+first_name=gets.chomp
+puts "Please Enter Your last name"
+last_name=gets.chomp
+name=first_name+" "+last_name
+puts "Welcome to Ruby "+ name
+10.times do
+  puts name
+end
