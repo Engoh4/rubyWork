a219f567f70cc679a808ed56a2cd9de8dc43615d
Engoh Lucienne <engohlucienne@gmail.com>
2020-03-17 11:02:25 +0100

    loops and iteration.Recursion too

---
+++ loop_iterators/conditional_loop.rb
@@ -0,0 +1,9 @@
+i=0
+loop do 
+  puts i
+  if i==10
+    break
+  end
+  i+=1
+  
+end
---
+++ loop_iterators/loop.rb
@@ -0,0 +1,6 @@
+input=""
+while input!="STOP" do
+  puts "Hi should i STOP?"
+  input=gets.chomp
+end
+puts "you said stop."
